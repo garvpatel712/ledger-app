76cd04a9c0e975cf5ee636769b91e08c7ed949b4
Patel Garv <garvpatel168@gmail.com>
2025-04-29 23:04:58 +0530

    frontend url added.

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'https://ledger-app-frontend.onrender.com', // Your frontend URL
   credentials: true
 }));
 app.use(express.json());
