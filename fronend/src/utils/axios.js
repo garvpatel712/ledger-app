7e035dc11d078a0d637f9237a360e47e97d67660
Patel Garv <garvpatel168@gmail.com>
2025-04-29 22:51:57 +0530

    backend deployed url added.

--- fronend/src/utils/axios.js
+++ fronend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ledger-app-backend.onrender.com/api',
   withCredentials: true
 });
 
